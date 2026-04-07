235af18b6b0fcad6107649bcb9a54b658a9454f1
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-07 15:59:22 +0200

    migrate(3.x): LLM-guided fixes for Elgg 3.x compatibility

---
+++ elgg-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'view_extensions' => [
+		'elements/components.css' => [
+			'elements/navigation/tabs.css' => [],
+		],
+		'elgg.js' => [
+			'elements/navigation/tabs.js' => [],
+		],
+	],
+];
